8040f6bd0af4dce720d71e543bda7c7ce945fd1c
Shubham  Bhade <121212493+shubhambhade@users.noreply.github.com>
2026-02-03 04:32:18 +0000

    2nd

--- blog_main/blog_main/views.py
+++ blog_main/blog_main/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render 
 
 def home(request):
-    return HttpResponse('<h2>HomePage</h2>')
+    return render(request, 'home.html')
